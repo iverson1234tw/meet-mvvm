49701645a6dd2e7ae5c86fe801ed7f92aba3067b
Josh Chen <josh@evaair.com>
2023-12-21 09:27:42 +0800

    start meeting

---
+++ MeetMVVM/Model/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  MeetMVVM
+//
+//  Created by Josh on 2023/12/21.
+//
+
+import UIKit
+
+struct UserModel {
+    let id: Int
+    let name: String
+    let phoneNumber: String
+    let address: String
+}
---
+++ MeetMVVM/ViewModel/UserViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  UserViewModel.swift
+//  MeetMVVM
+//
+//  Created by Josh on 2023/12/21.
+//
+
+import UIKit
+
+class UserViewModel {
+
+    // users init
+    var users: [UserModel] = []
+    
+    func fetchUser() {
+        
+        
+        
+    }
+    
+}
